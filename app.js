ea079b7e273802d90e63f4d681679a49bf5f023b
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-16 21:16:58 +0500

    while

---
+++ app.js
@@ -0,0 +1,55 @@
+// 1-masala
+
+// let a = 6;
+// let b = 10;
+// let sum = 1;
+
+// while(a <= b){
+//     sum *= a;
+//     a++;
+// }
+
+// console.log(sum);
+
+
+
+// 2-masala
+
+// let i = 7888;
+// let raqamsoni = 0;
+
+// while(i > 1){
+//     i = i / 10;
+//     raqamsoni += 1;
+// }
+
+// console.log(raqamsoni);
+
+
+
+// 3-masala
+
+// let n = 5;
+// let a = 1;
+
+// let i = 1;
+// while(i <= n){
+//     a *= i;
+//     i++;
+// }
+// console.log(a);
+
+
+// 4-masala
+
+// let parol = prompt("Parolni kiriting");
+// let password = 7777;
+
+// while(parol != password){
+//     parol = prompt("Parol notog'ri, qayta urunib ko'ring!");
+// }
+
+// while(parol == password){
+//     parol = alert("Paro'l tog'ri");
+// }
+
